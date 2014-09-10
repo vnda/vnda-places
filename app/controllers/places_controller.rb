804c2a45c478ab1a0798f7fa6600528b37b291c2
Francisco <francisco@vnda.com.br>
2014-09-10 16:53:43 +0000

    bug sucks

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -94,7 +94,8 @@ class PlacesController < ApplicationController
   private
 
   def authenticate
-    return true if @shop = Shop.find_by(host: (env['HTTP_X_STORE']).split(':').first) && env['HTTP_X_STORE'] != ""
+    puts ">>>> #{env['HTTP_X_STORE']}"
+    #return true if @shop = Shop.find_by(host: (env['HTTP_X_STORE'] || "unknown-host").split(':').first)
 
     if params[:format] == 'js'
       @shop = Shop.find_by!(token: params[:token])
